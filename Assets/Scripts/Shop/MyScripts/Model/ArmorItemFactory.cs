c6d60214b858975c99796c47b71c9943f7cd09df
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-04-25 17:17:58 +0200

    Armor item pocedural

--- Assets/Scripts/Shop/MyScripts/Model/ArmorItemFactory.cs
+++ Assets/Scripts/Shop/MyScripts/Model/ArmorItemFactory.cs
@@ -8,9 +8,32 @@ public class ArmorItemFactory : ItemAbstractFactory
     public ArmorItemFactory() { }
     public override MyItem MakeItem()
     {
-        ArmorItem newItem = new ArmorItem("Armour", "items_" + Random.Range(106, 109), TypeOfItem.Armor, 100);
-        newItem.SetArmorStats(Random.Range(80, 100), Random.Range(70, 90), Random.Range(10, 30));
-        newItem.SetUpgradePrice(Random.Range(20,50));
+        int chance = Random.Range(1, 100);
+        ArmorItem newItem;
+        if (chance < 30)
+        {
+            newItem = new ArmorItem("Armour", "items_" + 106, TypeOfItem.Armor, Random.Range(20, 50));
+            newItem.SetArmorStats(Random.Range(20, 30), Random.Range(10, 20), Random.Range(4, 10));
+            newItem.SetUpgradePrice(Random.Range(2, 10));
+        }
+        else if (chance < 55)
+        {
+            newItem = new ArmorItem("Armour", "items_" +107, TypeOfItem.Armor, Random.Range(50, 90));
+            newItem.SetArmorStats(Random.Range(30, 45), Random.Range(20, 40), Random.Range(10, 20));
+            newItem.SetUpgradePrice(Random.Range(10, 20));
+        }
+        else if (chance < 80)
+        {
+            newItem = new ArmorItem("Armour", "items_" + 108, TypeOfItem.Armor, Random.Range(90, 110));
+            newItem.SetArmorStats(Random.Range(50, 70), Random.Range(50, 70), Random.Range(20, 30));
+            newItem.SetUpgradePrice(Random.Range(20, 40));
+        }
+        else
+        {
+            newItem = new ArmorItem("Armour", "items_" + 109, TypeOfItem.Armor, Random.Range(110, 150));
+            newItem.SetArmorStats(Random.Range(80, 100), Random.Range(70, 90), Random.Range(30, 40));
+            newItem.SetUpgradePrice(Random.Range(40, 60));
+        }
         return newItem;
     }
 }
